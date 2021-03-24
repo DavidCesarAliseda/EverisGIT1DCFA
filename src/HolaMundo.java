78f15f56258273fc3f67ed97d996647bf5c5a7ff
DavidCesarAliseda <alum.dfernandeza.2020@iesalixar.org>
2021-03-24 17:12:51 +0100

    Version 1 - GIT

---
+++ src/HolaMundo.java
@@ -0,0 +1,15 @@
+/**
+ * Formación GIT
+ * 
+ * @author dcesarfe
+ */
+public class HolaMundo {
+	/**
+	 * Método principal 
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hola Mundo");
+	}
+}
